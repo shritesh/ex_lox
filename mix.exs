004798d652f8bbd9b0e18c37d5d70803789e2207
Shritesh Bhattarai <shr@ite.sh>
2021-02-18 10:00:19 -0500

    Run dialyzer in CI

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule ExLox.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
